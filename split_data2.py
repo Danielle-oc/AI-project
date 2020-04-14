887eb666bdfb25893a4050d8e6a3ee8be91b08d7
maya cohen <mayac@campus.technion.ac.il>
2020-04-14 13:40:41 +0300

    add split data

---
+++ split_data2.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from utils import save_to_csv_concat
+
+
+# TODO: change to tagged file
+data = pd.read_csv('labs_demo_tags_16_17.csv')
+
+
+labels = data['tag']
+features = data.drop(['tag'], axis=1)
+X_trainVal, X_test, y_trainVal, y_test = train_test_split(features, labels, test_size=0.25)
+X_train, X_val, y_train, y_val = train_test_split(X_trainVal, y_trainVal, test_size=0.2)
+
+
+save_to_csv_concat(X_train, y_train, 'train.csv')
+save_to_csv_concat(X_test, y_test, 'test.csv')
+save_to_csv_concat(X_val, y_val, 'val.csv')
+
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 import pandas as pd
-
+import numpy as np
 
 def save_to_csv(y, name):
     df = pd.DataFrame(y)
+    pd.DataFrame.to_csv(df, name, index=False)
+
+
+def save_to_csv_concat(X, y, name):
+    df = pd.concat([pd.DataFrame(y), X], axis=1)[np.append(['tag'], X.keys())]
     pd.DataFrame.to_csv(df, name, index=False)
