d6ac2e0a930edbf589586c3770bce0f8118580ce
Danielle <bossit2@gmail.com>
2020-04-15 13:00:53 +0300

    adding comments

--- date_format_demographic_oneHot.py
+++ date_format_demographic_oneHot.py
@@ -2,32 +2,43 @@ import pandas as pd
 import utils
 
 
+# formating all dates into timestamp format
 def format_date(file_name):
     data = pd.read_csv(file_name, encoding='utf8')
     columns_name = ["birth date", "aliya date", "death date"]
 
-    #ignores tabs and space in the "test date" column
     for col in columns_name:
-        #turns the "test date" column into timestamp format
+        # ignores tabs and space in the all date columns
         data[col] = data[col].str.strip()
+        # turns all date columns into timestamp format
         data[col] = pd.to_datetime(data[col], format='%d.%m.%Y', errors='coerce')
 
+    # ignores tabs and space in the "test date" column
     data["test date"] = data["test date"].str.strip()
-    #turns the "test date" column into timestamp format
+    # turns the "test date" column into timestamp format
     data["test date"] = pd.to_datetime(data["test date"], format='%d/%m/%Y')
     return data
 
 
-def oneHot(data):
+# turning all not numerical data into binary columns
+def one_hot(data):
     one_hot_df = data[["patient num", "test date", "birth country", "population", "gender", "clinic code"]]
+    # defining all columns that need to be transformed
     dummies = ["birth country", "population", "clinic code"]
+
+    # mapping the data into its real values
     one_hot_df['population'] = one_hot_df['population'].map({1: 'Jewish', 2: 'Arabic Muslim', 3: 'Muslim', 4: 'Arabic Christian', 5: 'Christian', 6: 'Druse', 9: 'else religion'})
     one_hot_df['gender'] = one_hot_df['gender'].map({1: '0', 2: '1'})
     one_hot_df['clinic code'] = one_hot_df['clinic code'].map({10: 'clalit', 11: 'macabi', 12: 'mehuedet', 13: 'leumit', 98: 'no clinic', 99: 'unkown clinic'})
+
+    # gender is already binary so don't need to transform
     data['gender'] = one_hot_df['gender']
+
+    # ANA pattern transform done previously but the original wasn't deleted
     data = data.drop('ANA Pattern', 1)
+
+    # transform all columns and setting it to data
     for field in dummies:
-        data[field] = one_hot_df[field]
         embarked_dummies1 = pd.get_dummies(one_hot_df[field])
         data = data.drop(field, 1)
         data = pd.concat([data, embarked_dummies1], axis=1)
@@ -35,11 +46,11 @@ def oneHot(data):
 
 
 data = format_date("train.csv")
-data = oneHot(data)
+data = one_hot(data)
 utils.save_to_csv(data, "train_organized.csv")
 data = format_date("test.csv")
-data = oneHot(data)
+data = one_hot(data)
 utils.save_to_csv(data, "test_organized.csv")
 data = format_date("val.csv")
-data = oneHot(data)
+data = one_hot(data)
 utils.save_to_csv(data, "val_organized.csv")
--- normalization.py
+++ normalization.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy
 
 
-#check if its Gaussian by Shapiro method
+# check if its Gaussian by Shapiro method
 def check_normalization(feature_set):
     feature_set = numpy.asarray(feature_set)
     k2, p = stats.shapiro(feature_set)
@@ -18,20 +18,20 @@ def check_normalization(feature_set):
 
 def normalization_handler(file_name):
     data = pd.read_csv(file_name, encoding='utf8')
-    #data that shouldn't be normalized
+    # data that shouldn't be normalized
     not_normalized_column = {"patient num", "test date",	"birth date",	"gender",
                            	"death date",	"aliya date",	"tag"}
     normalized_data = data[["patient num", "test date", "birth date", "gender",
                             "death date",	"aliya date", "tag"]]
 
-    #initializing scalers
+    # initializing scalers
     standart_scaler = StandardScaler()
     minmax_scaler = MinMaxScaler()
 
     standard_features = []
     minmax_features = []
 
-    #check which feature is gaussian and which is uniformic
+    # check which feature is gaussian and which is uniformic
     for (col_name, col_data) in data.iteritems():
         if col_name not in not_normalized_column:
             normalized = check_normalization(col_data)
@@ -40,26 +40,26 @@ def normalization_handler(file_name):
             else:
                 minmax_features.append(col_name)
 
-    #scaling all the gaussian features
+    # scaling all the gaussian features
     for i in range(len(standard_features)):
         feature = standard_features[i]
         scaled_df = standart_scaler.fit_transform((numpy.array(data[feature])).reshape(-1, 1))
         scaled_df = pd.DataFrame(scaled_df)
 
-        #check if the feature was really gaussian, if not doing minmax scaling
+        # check if the feature was really gaussian, if not doing minmax scaling
         for j in scaled_df[0]:
             if j > 1:
                 scaled_df = minmax_scaler.fit_transform((numpy.array(data[feature])).reshape(-1, 1))
                 break
         normalized_data[feature] = scaled_df
 
+    # scaling all the uniformian features
     for i in range(len(minmax_features)):
         feature = minmax_features[i]
         scaled_df = minmax_scaler.fit_transform((numpy.array(data[feature])).reshape(-1, 1))
         scaled_df = pd.DataFrame(scaled_df)
         normalized_data[feature] = scaled_df
-    #normalized_data[standard_features] = standart_scaler.fit_transform(data[standard_features])
-    #normalized_data[minmax_features] = minmax_scaler.fit_transform(data[minmax_features])
+
     return normalized_data
 
 
