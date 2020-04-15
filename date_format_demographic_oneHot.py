cd38a2c5a5ba5324de573fdcf40d4929fef1a92e
Danielle <bossit2@gmail.com>
2020-04-15 12:00:55 +0300

    organizing data: formating dates and oneHot to all not numeric values

--- date_format_demographic_oneHot.py
+++ date_format_demographic_oneHot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import utils
+import numpy as np
 
 
 def format_date(file_name):
@@ -11,14 +12,35 @@ def format_date(file_name):
         #turns the "test date" column into timestamp format
         data[col] = data[col].str.strip()
         data[col] = pd.to_datetime(data[col], format='%d.%m.%Y', errors='coerce')
-        for i in range(len(data["birth date"])):
-            if data[col][i] in (None, ""):
-                data[col][i] = '00/00/0000'
+
     data["test date"] = data["test date"].str.strip()
     #turns the "test date" column into timestamp format
     data["test date"] = pd.to_datetime(data["test date"], format='%d/%m/%Y')
-    file_name = file_name.split('.')[0]
-    utils.save_to_csv(data, file_name + "_organized.csv")
+    return data
 
 
-format_date("train.csv")
+def oneHot(data):
+    one_hot_df = data[["patient num", "test date", "birth country", "population", "gender", "clinic code"]]
+    dummies = ["birth country", "population", "clinic code"]
+    one_hot_df['population'] = one_hot_df['population'].map({1: 'Jewish', 2: 'Arabic Muslim', 3: 'Muslim', 4: 'Arabic Christian', 5: 'Christian', 6: 'Druse', 9: 'else religion'})
+    one_hot_df['gender'] = one_hot_df['gender'].map({1: '0', 2: '1'})
+    one_hot_df['clinic code'] = one_hot_df['clinic code'].map({10: 'clalit', 11: 'macabi', 12: 'mehuedet', 13: 'leumit', 98: 'no clinic', 99: 'unkown clinic'})
+    data['gender'] = one_hot_df['gender']
+    data = data.drop('ANA Pattern', 1)
+    for field in dummies:
+        data[field] = one_hot_df[field]
+        embarked_dummies1 = pd.get_dummies(one_hot_df[field])
+        data = data.drop(field, 1)
+        data = pd.concat([data, embarked_dummies1], axis=1)
+    return data
+
+
+data = format_date("train.csv")
+data = oneHot(data)
+utils.save_to_csv(data, "train_organized.csv")
+data = format_date("test.csv")
+data = oneHot(data)
+utils.save_to_csv(data, "test_organized.csv")
+data = format_date("val.csv")
+data = oneHot(data)
+utils.save_to_csv(data, "val_organized.csv")
