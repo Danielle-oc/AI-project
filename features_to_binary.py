36da41f3f131fd9b7c2b5fb2b3a3b16f966612b7
Danielle <bossit2@gmail.com>
2020-04-17 15:51:43 +0300

    Turn columns to binary columns, if the test was done or not.

---
+++ features_to_binary.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import utils
+
+def get_feature_in_binary(file_name):
+    data = pd.read_csv(file_name, encoding='utf8')
+    null_list = data.columns[data.isna().any()].tolist()
+    no_binary = ['death date', 'aliya date']
+    list_to_binary = list(set(null_list) - set(no_binary))
+    for feature in list_to_binary:
+        data[feature] = data[feature].apply(lambda x: 1 if not pd.isna(x) else 0)
+    return data
+
+# data = get_feature_in_binary("normalized_train_data_16_17.csv")
+# utils.save_to_csv(data, "train_feature_to_binary_data_16_17.csv")
+data = get_feature_in_binary("normalized_test_data_16_17.csv")
+utils.save_to_csv(data, "test_feature_to_binary_data_16_17.csv")
+data = get_feature_in_binary("normalized_val_data_16_17.csv")
+utils.save_to_csv(data, "val_feature_to_binary_data_16_17.csv")
